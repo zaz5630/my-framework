d40f884f6d93ecb517c27790d119fe0bbcf6224a
zaz5630 <670049410@qq.com>
2021-03-15 22:30:31 +0800

    add

---
+++ gmall0820-parent/gmall0820-publisher/src/main/java/com/atguigu/gmall/service/VisitorStatsService.java
@@ -0,0 +1,18 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.VisitorStats;
+
+import java.util.List;
+
+/**
+ * Author: Felix
+ * Date: 2021/2/27
+ * Desc: 访客统计业务层接口
+ */
+public interface VisitorStatsService {
+
+    List<VisitorStats> getVisitorStatsByNewFlag(int date);
+
+    List<VisitorStats> getVisitorStatsByHr(int date);
+
+}
